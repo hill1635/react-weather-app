f9fb5a2db000005fe023bea12a9daecf1763389b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-06-26 13:49:41 -0600

    Delete user function added.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,4 +4,7 @@ export default {
     createUser: function(userData) {
         return axios.post("/api/users", userData);
     },
+    deleteUser: function(id) {
+        return axios.delete("api/users/" + id);
+    },
 };
