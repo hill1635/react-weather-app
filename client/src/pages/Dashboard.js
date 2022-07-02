9c677fb9cdac493af52c87ab5f12538a2671a89a
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 14:17:27 -0600

    Dashboard elements built out.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,19 @@
 import React from "react";
+import SaveBtn from "../components/SaveBtn";
+import CancelBtn from "../components/CancelBtn";
 
 function Dashboard() {
-    return (
-        <div>
-            <h1>Welcome to your Dashboard!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to your Dashboard!</h1>
+      <div className="search">
+        <input type="text" placeholder="Search"></input>
+        <SaveBtn />
+        <CancelBtn />
+      </div>
+      <div className="saved"></div>
+    </div>
+  );
 }
 
 export default Dashboard;
