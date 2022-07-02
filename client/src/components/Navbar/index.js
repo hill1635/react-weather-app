68f354da35cb72202e7e95b14af9bb6ca5601bb7
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 12:02:33 -0600

    Adjusted Navbar structure.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,41 +2,43 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <span>
-            <h1 className="title">Weather App</h1>
-          </span>
-  
-          <button
-            className="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </button>
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <span>
+          <h1 className="title">Weather App</h1>
+        </span>
+
+        <button
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </button>
+      </div>
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <Link to="/" className="navbar-item">
+            Dashboard
+          </Link>
         </div>
-  
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <Link to="/" className="navbar-item">Dashboard</Link>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
             </div>
           </div>
-  
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Sign Up</Link>
-              </div>
-            </div>
-          </div>
-      </nav>
-    );
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
