67ee67540f244eeaf21296139d7205d29224bd5f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 14:20:06 -0600

    FiveDayDiv created.

---
+++ client/src/components/FiveDayDiv/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function FiveDayDiv() {
+    return (
+        <div className="fiveDay">
+            <span>Day One</span>
+            <span>Day Two</span>
+            <span>Day Three</span>
+            <span>Day Four</span>
+            <span>Day Five</span>
+        </div>
+    );
+}
+
+export default FiveDayDiv;
