7490726ae4d1ff10f43ae22f3d2aa292373212df
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 17:25:06 -0600

    Navbar basic styling.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./style.css";
 
 function Navbar() {
   const [menuState, setMenuState] = useState("disabled");
@@ -9,7 +10,7 @@ function Navbar() {
     var dropdownBtn = document.querySelector(".navbar-burger");
     var dropdownMenu = document.querySelector(".navbar-menu");
 
-    dropdownBtn.addEventListener("click", function() {
+    dropdownBtn.addEventListener("click", function () {
       if (state == "disabled") {
         dropdownMenu.classList.add("is-active");
         setMenuState("enabled");
@@ -24,17 +25,18 @@ function Navbar() {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <span>
-          <h1 className="title">Weather App</h1>
+        <span className="title">
+          <Link to="/">
+          <h1>Weather App</h1>
+          </Link>
         </span>
 
         <a
           role="button"
-          className="navbar-burger"
+          className="cloud navbar-burger"
           aria-label="menu"
           aria-expanded="false"
           data-target="navbarBasicExample"
-          
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -44,22 +46,25 @@ function Navbar() {
 
       <div id="navbarBasicExample" className="navbar-menu">
         <div className="navbar-start">
-          <Link to="/" className="navbar-item">
-            Home
-          </Link>
         </div>
 
         <div className="navbar-end">
-          <div className="navbar-item">
             <div className="buttons">
-              <Link to="/dashboard">Dashboard</Link>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-              <Link to="/account">Account</Link>
+              <div className="navBtn">
+                <Link to="/dashboard" className="navbar-item p-0">Dashboard</Link>
+              </div>
+              <div className="navBtn">
+                <Link to="/login" className="navbar-item p-0">Login</Link>
+              </div>
+              <div className="navBtn">
+                <Link to="/signup" className="navbar-item p-0">Sign Up</Link>
+              </div>
+              <div className="navBtn">
+                <Link to="/account" className="navbar-item p-0">Account</Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
     </nav>
   );
 }
