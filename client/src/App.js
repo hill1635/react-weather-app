52d94f0ee2bbad6dbab0b30107e378d1c684f62c
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 14:44:41 -0600

    Footer added to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,23 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Account from "./pages/Account";
+import Footer from "./components/Footer";
 import "bulma/css/bulma.min.css";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="background">
-        <Navbar/>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/dashboard" element={<Dashboard />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<SignUp />} />
-            <Route exact path="/account" element={<Account />} />
-          </Routes>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/account" element={<Account />} />
+        </Routes>
+        <Footer />
       </div>
     </Router>
   );
