3437a81720ea94df0f8e4ac4d87430b2b8493bc3
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 11:53:51 -0600

    SignupBtn imported.

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SignupBtn from "../components/SignupBtn";
 
 function SignUp() {
   return (
@@ -6,6 +7,7 @@ function SignUp() {
       <h1>Sign Up</h1>
       <input type="text" placeholder="Username"></input>
       <input type="text" placeholder="Password"></input>
+      <SignupBtn />
     </div>
   );
 }
