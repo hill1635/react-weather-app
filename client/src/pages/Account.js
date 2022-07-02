98031dc4a569c448ed880fb233b2dba019ffc7ee
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 13:47:03 -0600

    Account page skeleton created.

---
+++ client/src/pages/Account.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Account() {
+  return (
+    <div>
+      <h1>Account Info</h1>
+      <p>First:</p>
+      <p>Last:</p>
+      <p>Email:</p>
+      <p>Location:</p>
+    </div>
+  );
+}
+
+export default Account;
