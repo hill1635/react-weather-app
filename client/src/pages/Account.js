362bc5c648f98bdaed19bf628eb76ee46b2a8dc4
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 14:06:33 -0600

    EditBtn added to Account page.

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,13 +1,14 @@
 import React from "react";
+import EditBtn from "../components/EditBtn";
 
 function Account() {
   return (
     <div>
-      <h1>Account Info</h1>
-      <p>First:</p>
-      <p>Last:</p>
-      <p>Email:</p>
-      <p>Location:</p>
+      <h1>Account Info</h1><EditBtn />
+      <p>First:</p><EditBtn />
+      <p>Last:</p><EditBtn />
+      <p>Email:</p><EditBtn />
+      <p>Location:</p><EditBtn />
     </div>
   );
 }
