92e9b57f3b9f0afa921e841a68e9725a7a235e6a
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 11:41:55 -0600

    Login page added to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 import "bulma/css/bulma.min.css";
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
         <Navbar/>
           <Routes>
             <Route exact path="/" component={Dashboard} />
+            <Route exact path="/login" component={Login} />
           </Routes>
       </div>
     </Router>
