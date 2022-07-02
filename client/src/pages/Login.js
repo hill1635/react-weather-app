53f3dd23580a5f785d91a8d3ff90d47ac3b8f108
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 11:38:18 -0600

    Login page created.

---
+++ client/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <input type="text" placeholder="Username"></input>
+            <input type="text" placeholder="Password"></input>
+        </div>
+    );
+}
+
+export default Login;
