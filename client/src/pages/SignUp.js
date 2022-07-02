5973dd1396e6f82279409c1c958a89df34f127b5
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 11:53:10 -0600

    SignUp page created.

---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <input type="text" placeholder="Username"></input>
+      <input type="text" placeholder="Password"></input>
+    </div>
+  );
+}
+
+export default SignUp;
