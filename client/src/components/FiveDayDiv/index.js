91f06f7660e660f457cee80ca7d35ef185e963ae
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-03 13:24:18 -0600

    Changed fiveDayDiv key value to location id.

--- client/src/components/FiveDayDiv/index.js
+++ client/src/components/FiveDayDiv/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import DeleteBtn from "../DeleteBtn";
 
 function FiveDayDiv(props) {
-var propsArray = [...props.locations];
 
   return (
     <div className="fiveDayDiv">
       {props.locations.map((location) => (
-        <div key={props.locations.indexOf(location)}>
+        <div key={props.locations.id}>
           <h1>{location.name}</h1>
           <span>Day One</span>
           <span>Day Two</span>
