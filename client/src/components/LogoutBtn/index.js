d445f8e2536b319f8b50db6a424855b83ca878f5
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 21:26:57 -0600

    endSession function added to LogoutBtn.

--- client/src/components/LogoutBtn/index.js
+++ client/src/components/LogoutBtn/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import API from "../../utils/API";
 
 function LogoutBtn() {
-    return (
-        <button>Log Out</button>
-    );
+  var endSession = () => {
+    API.logout()
+    .then(() => console.log("logged out"));
+  };
+
+  return <button onClick={endSession}>Log Out</button>;
 }
 
 export default LogoutBtn;
