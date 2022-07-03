30481ce9580a8619bd2373063547a5e409da00c3
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-02 21:30:40 -0600

    LogoutBtn relocates to Login page.

--- client/src/components/LogoutBtn/index.js
+++ client/src/components/LogoutBtn/index.js
@@ -4,7 +4,7 @@ import API from "../../utils/API";
 function LogoutBtn() {
   var endSession = () => {
     API.logout()
-    .then(() => console.log("logged out"));
+    .then(() => window.location.href="/login");
   };
 
   return <button onClick={endSession}>Log Out</button>;
