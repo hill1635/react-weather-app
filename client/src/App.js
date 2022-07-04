498c330e74615d831c95a148f093b6a90b673f5a
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-04 15:27:19 -0600

    Moved Home to own folder.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/Home";
+import Home from "./home/Home";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
