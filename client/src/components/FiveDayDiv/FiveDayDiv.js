27185b7af505b08d83a010b468857be0c72c45ff
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-07-25 12:57:36 -0600

    Deleted template literal.

--- client/src/components/FiveDayDiv/FiveDayDiv.js
+++ client/src/components/FiveDayDiv/FiveDayDiv.js
@@ -9,17 +9,6 @@ function FiveDayDiv(props) {
       var root = document.querySelector(".fiveDayDiv");
 
       props.dailyForecast.forEach((day) => {
-      //   const markup = `
-      //   <div className="dayDiv">
-      //   <h5>${moment.unix(day.dt).format("dddd")}</h5>
-      //   <img src="http://openweathermap.org/img/wn/${
-      //     day.weather[0].icon
-      //   }@2x.png"</img>
-      //   <span>${day.temp.max}</span>
-      //   <span>${day.temp.min}</span>
-      //   </div>
-      // `;
-      // root.innerHTML += markup;
       var div = document.createElement("div");
       var header = document.createElement("h4");
       var img = document.createElement("img");
