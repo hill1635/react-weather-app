d56c4e80a95302258c5c6579c71e276812f54672
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-04-15 20:11:16 -0600

    Flexbox styling to Main component.

--- client/src/home/components/Main.js
+++ client/src/home/components/Main.js
@@ -3,13 +3,17 @@ import React from "react";
 function Main() {
   return (
       <section className="currentMain">
-        <h1>Salt Lake City</h1>
-        <h4><span id="currentTime"></span></h4>
-        <h4>Current <span id="currentTemp"></span></h4>
-        <h5>Feels Like <span id="feelsLikeTemp"></span></h5>
-        <h6>High <span id="highTemp"></span></h6>
-        <h6>Low <span id="lowTemp"></span></h6>
-        <img id="icon" src=""></img>
+        <h1 className="currentHeader">Salt Lake City</h1>
+        <div className="currentWeather">
+          <img className="weatherIcon" id="icon" src=""></img>
+          <div className="currentDetails">
+            <h4><span id="currentTime"></span></h4>
+            <h4>Current <span id="currentTemp"></span></h4>
+            <h5>Feels Like <span id="feelsLikeTemp"></span></h5>
+            <h6>High <span id="highTemp"></span></h6>
+            <h6>Low <span id="lowTemp"></span></h6>
+          </div>
+        </div>
       </section>
   );
 }
