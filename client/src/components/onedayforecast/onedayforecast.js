db4b1c73093ed19f926bd65c7a312d9c0335ce86
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-05-20 12:28:23 -0600

    Created OneDayForecast component.

---
+++ client/src/components/onedayforecast/onedayforecast.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function OneDayForecast() {
+  return <div className="oneDay">
+    <div className="oneDayName"></div>
+    <div className="oneDayIcon"></div>
+    <div className="oneDayHigh"></div>
+    <div className="oneDayLow"></div>
+  </div>;
+}
+
+export default OneDayForecast;
