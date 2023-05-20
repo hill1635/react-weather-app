97ab2f3ccdf0f1d6eb108e36c82302220a6559e9
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-05-20 17:48:56 -0600

    Prop drilled daily forecast information.

--- client/src/components/onedayforecast/onedayforecast.js
+++ client/src/components/onedayforecast/onedayforecast.js
@@ -1,13 +1,18 @@
 import React from "react";
+import moment from "moment";
 
 function OneDayForecast(props) {
-  console.log("OneDayForecast:", props);
-  return <div className="oneDay">
-    <div className="oneDayName"></div>
-    <div className="oneDayIcon"></div>
-    <div className="oneDayHigh"></div>
-    <div className="oneDayLow"></div>
-  </div>;
+  if (Object.keys(props.day).length > 0) {
+    var day = props.day;
+    return (
+      <div className="oneDay">
+        <div className="oneDayName">{moment.unix(day.dt).format("ddd")}</div>
+        <div className="oneDayIcon"></div>
+        <div className="oneDayHigh">{Math.round(day.temp.max) + "°F"}</div>
+        <div className="oneDayLow">{Math.round(day.temp.min) + "°F"}</div>
+      </div>
+    );
+  }
 }
 
 export default OneDayForecast;
