89c6c451381a995059b102dd29a5ac0fb5c7ae5f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-05-20 17:02:13 -0600

    Prop drilled forecast data and conditionally rendered Additional component instead of dynamically adding HTML. Cleaned up unused code from useEffect.

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import moment from "moment";
 import Main from "./components/Main";
 import Additional from "./components/Additional";
 import FiveDayDiv from "../components/fivedaydiv/FiveDayDiv";
@@ -14,51 +13,19 @@ function Home(props) {
     document.querySelector(tag).innerHTML = value;
   };
 
-  const degToCompass = (num) => {
-    var degree = parseInt((num/22.5)+.5);
-    var directions = ["N", "NE", "NNE", "ENE", "E", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
-    return directions[(degree % 16)];
-  };
-
   // var getAQI = () => {
   //   API.getAQI(lat, long)
   //     .then((res) => console.log("AQI: ", res.data));
   // };
+  
   useEffect(() => {
-    var updateWeather = (data) => {
-      setForecast(data);
-      // var current = forecast.current;
-      // var daily = forecast.daily[0];
-      // var apiData = [data];
-      // console.log("data:", data);
-      // apiData[0].daily.shift();
-
-      // // ? How to get these divided out to separate components
-      // document.querySelector("#icon").src = "http://openweathermap.org/img/wn/" + daily.weather[0].icon + "@2x.png";
-      // updateHTML("#currentTemp", Math.round(current.temp) + "&#8457");
-      // updateHTML("#feelsLikeTemp", Math.round(current.feels_like) + "&#8457");
-      // updateHTML("#highTemp", Math.round(daily.temp.max) + "&#8457");
-      // updateHTML("#lowTemp", Math.round(daily.temp.min) + "&#8457");
-
-      // updateHTML("#humid", current.humidity + "%");
-      // updateHTML("#uvi", current.uvi);
-
-      // updateHTML("#windSpeed", Math.round(current.wind_speed) + "mph");
-      // updateHTML("#windDirection", degToCompass(current.wind_deg));
-
-      // updateHTML("#sunrise", moment.unix(current.sunrise).format("LT"));
-      // updateHTML("#sunset", moment.unix(current.sunset).format("LT"));
-      // updateHTML("#moonphase", daily.moon_phase);
-    };
-
-    // getAQI();
-    props.getWeather(latitude, longitude, updateWeather);
+    props.getWeather(latitude, longitude, setForecast);
   }, [latitude, longitude, props]);
 
   return (
     <main className="weatherWrapper">
           <Main forecast={forecast}/>
-          <Additional />
+          <Additional forecast={forecast}/>
       <section className="hourlyForecast col-12 mx-auto mb-5">
         Hourly forecast
       </section>
--- client/src/home/components/Additional.js
+++ client/src/home/components/Additional.js
@@ -1,22 +1,33 @@
 import React from "react";
+import moment from "moment";
 
-function Additional() {
+function Additional(props) {
+
+  const degToCompass = (num) => {
+    var degree = parseInt((num/22.5)+.5);
+    var directions = ["N", "NE", "NNE", "ENE", "E", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    return directions[(degree % 16)];
+  };
+
+  if (Object.keys(props.forecast).length > 0) {
+    var forecast = props.forecast;
     return (
         <section className="currentAdd">
           <div className="addDiv">
-            <p>Humidity: <span id="humid"></span></p>
+            <p>Humidity: <span id="humid">{forecast.current.humidity + "%"}</span></p>
             <p>Air Quality: <span id="aqi"></span></p>
-            <p>Wind Speed: <span id="windSpeed"></span></p>
-            <p>Wind Direction: <span id="windDirection"></span></p>
+            <p>Wind Speed: <span id="windSpeed">{Math.round(forecast.current.wind_speed) + "mph"}</span></p>
+            <p>Wind Direction: <span id="windDirection">{degToCompass(forecast.current.wind_deg)}</span></p>
           </div>
           <div className="addDiv">
-            <p>UV Index: <span id="uvi"></span></p>
-            <p>Sunrise <span id="sunrise"></span></p>
-            <p>Sunset <span id="sunset"></span></p>
-            <p>Moon Phase <span id="moonphase"></span></p>
+            <p>UV Index: <span id="uvi">{forecast.current.uvi}</span></p>
+            <p>Sunrise <span id="sunrise">{moment.unix(forecast.current.sunrise).format("LT")}</span></p>
+            <p>Sunset <span id="sunset">{moment.unix(forecast.current.sunset).format("LT")}</span></p>
+            <p>Moon Phase <span id="moonphase">{forecast.daily.moon_phase}</span></p>
           </div>
       </section>
     );
+  }
 }
 
 export default Additional;
