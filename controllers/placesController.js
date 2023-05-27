6250f25dc025094c0a21c3e2ba2aa86c9b1f32f5
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-05-27 12:26:01 -0600

    Created and connected places routes and controller.

---
+++ controllers/placesController.js
@@ -0,0 +1,5 @@
+const db = require("../models/places");
+
+module.exports = {
+
+};
---
+++ routes/api/places.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const placeController = require("../../controllers/placeController");
+
+module.exports = router;
