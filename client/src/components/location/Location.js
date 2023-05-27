82be06c2ed3c1b4e709a51fe3c7447aa88094343
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-05-27 13:25:38 -0600

    Created Location component.

---
+++ client/src/components/location/Location.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Location.css";
+
+function Location() {
+    return (
+        <div className="location">
+            <h3>Location</h3>
+            <div className="locationForecast">
+            </div>
+        </div>
+    );
+}
+
+export default Location;
