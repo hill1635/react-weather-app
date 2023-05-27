12fb5b5ecab8a8e46d090839c3645731b06b8265
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-05-27 15:44:40 -0600

    Imported Location component into LocationsDB.

--- client/src/components/location/Location.js
+++ client/src/components/location/Location.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Location.css";
 
-function Location() {
+function Location(props) {
+    console.log("Location:", props.data);
+    if (Object.keys(props.data).length > 0) {
     return (
         <div className="location">
             <h3>Location</h3>
@@ -9,6 +11,7 @@ function Location() {
             </div>
         </div>
     );
+    }
 }
 
 export default Location;
--- client/src/savedlocations/components/LocationsDB.js
+++ client/src/savedlocations/components/LocationsDB.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
-import ExtendedForecast from "../../components/extendedForecast/ExtendedForecast";
+import Location from "../../components/location/Location";
 
 
-function LocationsDB() {
-    var forecasts = [];
-    return (
-        <div className="saved">
-        {/* {forecasts.map((forecast) => {
-            return <ExtendedForecast forecast={forecast} />
-        })} */}
-      </div>
-    );
+function LocationsDB(props) {
+    if (Object.keys(props.locations).length > 0) {
+        return (
+            <div className="saved">
+            {props.locations.map((locationData) => {
+                return <Location data={locationData} />
+            })}
+        </div>
+        );
+    }
 }
 
 export default LocationsDB;
