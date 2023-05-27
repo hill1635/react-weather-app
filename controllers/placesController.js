e18ae3370c95e9b3456e85b4393975b6880d920e
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-05-27 12:44:18 -0600

    Added placesController functions to create, get remove, update forecast and update historical data for place.

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -1,5 +1,33 @@
 const db = require("../models/places");
 
 module.exports = {
-
+    create: function (req, res) {
+        console.log("create: ", req.body);
+        // db.create(req.body)
+        //     .then((dbModel) => res.json(dbModel))
+        //     .catch((err) => res.status(422).json(err));
+    },
+    get: function (req, res) {
+        console.log("getLocations: ", req.body);
+        // db.find({ _id: req.session.placeId })
+        //     .then((dbModel) => { res.json(dbModel) })
+        //     .catch((err) => res.status(500).json(err));
+    },
+    remove: function (req, res) {
+        console.log("remove: ", req.body);
+        // db.findById({ _id: req.session.placeId }, req.body)
+            // .then((dbModel) => dbModel.remove())
+            // .then((dbModel) => res.json(dbModel))
+            // .catch((err) => res.status(422).json(err));
+    },
+    updateForecast: function (req, res) {
+        console.log("updateForecast: ", req.body);
+        // db.findOneAndUpdate(req.session._id, { forecast: req.params.id })
+            // .catch((err) => res.status(500).json(err));
+    },
+    updateHistorical: function (req, res) {
+        console.log("updateHistorical:", req.body);
+        // db.findOneAndUpdate(req.session._id, { historical: req.params.id })
+        //     .catch((err) => res.status(500).json(err));
+    },
 };
