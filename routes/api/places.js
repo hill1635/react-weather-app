5d9e4e04de7cefb57ac0cfc9c638e38df369881a
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-05-27 12:57:28 -0600

    Set up places api routes.

--- routes/api/places.js
+++ routes/api/places.js
@@ -1,4 +1,21 @@
 const router = require("express").Router();
 const placeController = require("../../controllers/placeController");
 
+router
+    .route("/")
+    .post(placeController.create);
+
+router
+    .route("/:id")
+    .get(placeController.get);
+
+router
+    .route("/:id")
+    .put(placeController.updateForecast);
+
+
+router
+    .route("/:id")
+    .put(placeController.updateHistorical);
+
 module.exports = router;
