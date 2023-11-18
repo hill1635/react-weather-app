c8488eecd231a7ae6f1b456ace2b0b36577c6048
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2023-11-18 13:04:51 -0700

    App.scss set up with rule skeleton.

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import "./App.css";
+import "./App.scss";
 
 function App() {
   const [ status, setStatus ] = useState(false);
